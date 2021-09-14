6902cc2e8d1f4c8ba94f7acdccf8681be11302da
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-09-14 16:46:39 -0300

    Requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div
+        data-testid="page-login"
+      >
+        Login
+      </div>
+
+    );
+  }
+}
