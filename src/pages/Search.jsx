e4d475405f149d41360bddf1d60ce5b84d52c2e4
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-09-17 10:22:25 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,48 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput({ target }) {
+    console.log('oi');
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { name } = this.state;
+    const numMinCaracter = 2;
     return (
       <div data-testid="page-search">
-        Pesquisar
+        Pesquisa
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="name"
+            placeholder="Nome do artista"
+            onChange={ this.handleInput }
+            d
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ name.length < numMinCaracter }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
